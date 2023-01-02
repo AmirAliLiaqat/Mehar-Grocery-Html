f92a7a8fe1b1ff4f3b5dead7d496dec8e5588c90
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-02 20:50:57 +0500

    Editing the site main footer section...

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -40,11 +40,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                        &copy; <a href="#">Mehar Grocery</a>, All Right Reserved.
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://phpcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a href="https://phpcodex.com">php Codex</a>
+                        Designed By <a href="https://bytekiduniya.com">Byte Ki Duniya</a>
                     </div>
                 </div>
             </div>
