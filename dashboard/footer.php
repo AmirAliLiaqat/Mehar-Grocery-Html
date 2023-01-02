f242cb5cfb9838f0a0bbd2d221c62c221c2bc340
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-02 20:48:19 +0500

    Editing the admin footer section...

---
+++ dashboard/footer.php
@@ -0,0 +1,13 @@
+<footer class="border-top" style="background: var(--primary);">
+    <div class="container-fluid d-flex justify-content-between p-3">
+        <div class="copyright">
+            <p class="text-white mb-0">Copyright © 2023. All Rights Reserved.</p>
+        </div><!--copyright-->
+        <div class="date_time">
+            <a href="#" class="mx-1"><i class="fa-brands fa-facebook-f btn btn-light text-primary rounded-circle"></i></a>
+            <a href="#" class="mx-1"><i class="fa-brands fa-twitter btn btn-light text-info rounded-circle"></i></a>
+            <a href="#" class="mx-1"><i class="fa-brands fa-instagram btn btn-light text-danger rounded-circle"></i></a>
+            <a href="#" class="mx-1"><i class="fa-brands fa-linkedin-in btn btn-light text-primary rounded-circle"></i></a>
+        </div><!--date_time-->
+    </div><!--container-->
+</footer>
