635bde84515ba47c653c4f8b790a03b71827f5ec
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-02 19:41:51 +0500

    Adding font awesome cdn...

--- template-parts/header-links.php
+++ template-parts/header-links.php
@@ -19,3 +19,6 @@
 
 <!-- Template Stylesheet -->
 <link href="css/style.css" rel="stylesheet">
+
+<!-- Font Awesome Css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
