adc3699565d4f551cdba4bf38fc99bd748e09629
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-03 18:29:02 +0500

    Editing the main navbar....

--- template-parts/header.php
+++ template-parts/header.php
@@ -33,15 +33,16 @@
                 <a href="index.php" class="nav-item nav-link active">Home</a>
                 <a href="about.php" class="nav-item nav-link">About Us</a>
                 <a href="product.php" class="nav-item nav-link">Products</a>
-                <div class="nav-item dropdown">
+                <a href="blog.php" class="nav-item nav-link">Blog</a>
+                <!-- <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu m-0">
                         <a href="blog.php" class="dropdown-item">Blog Grid</a>
                         <a href="feature.php" class="dropdown-item">Our Features</a>
                         <a href="testimonial.php" class="dropdown-item">Testimonial</a>
                         <a href="404.php" class="dropdown-item">404 Page</a>
-                    </div><!--dropdown-menu-->
-                </div><!--nav-item-->
+                    </div>
+                </div> -->
                 <a href="contact.php" class="nav-item nav-link">Contact Us</a>
             </div><!--navbar-nav-->
             <div class="d-none d-lg-flex ms-2">
