65ac3e9f96aa50e87ac00290a78fea808aaadecf
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-04 14:11:13 +0500

    Adding two new pages in menu...

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,5 +1,6 @@
 <ul class="menu-list">
     <li <?php if(basename($_SERVER['PHP_SELF']) == "dashboard.php") echo 'class="active"'; ?>><a href="dashboard.php">Dashboard</a></li>
+    <li <?php if(basename($_SERVER['PHP_SELF']) == "posts.php") echo 'class="active"'; ?>><a href="posts.php">Posts</a></li>
     <li <?php if(basename($_SERVER['PHP_SELF']) == "products.php") echo 'class="active"'; ?>><a href="products.php">Products</a></li>
     <li <?php if(basename($_SERVER['PHP_SELF']) == "orders.php") echo 'class="active"'; ?>><a href="orders.php">Orders</a></li>
     <li <?php if(basename($_SERVER['PHP_SELF']) == "users.php") echo 'class="active"'; ?>><a href="users.php">Users</a></li>
@@ -8,4 +9,5 @@
     <li <?php if(basename($_SERVER['PHP_SELF']) == "categories.php") echo 'class="active"'; ?>><a href="categories.php">Categories</a></li>
     <li <?php if(basename($_SERVER['PHP_SELF']) == "sub-categories.php") echo 'class="active"'; ?>><a href="sub-categories.php">Sub Categories</a></li>
     <li <?php if(basename($_SERVER['PHP_SELF']) == "options.php") echo 'class="active"'; ?>><a href="options.php">Options</a></li>
+    <li <?php if(basename($_SERVER['PHP_SELF']) == "profile.php") echo 'class="active"'; ?>><a href="profile.php">Profile</a></li>
 </ul>
