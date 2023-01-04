a08bc22287eb3442c8d33fc8e178d3b9e35b284d
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-04 21:04:17 +0500

    Converting all blog posts custom to dynamic...

--- index.php
+++ index.php
@@ -754,41 +754,32 @@
             <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
                 <h1 class="display-5 mb-3">Latest Blog</h1>
                 <p>Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
-            </div>
+            </div><!--section-header-->
             <div class="row g-4">
+                <?php
+                    $fetch_blog_posts = "SELECT * FROM `posts` WHERE `status` = 'publish' LIMIT 3";
+                    $fetch_blog_posts_query = mysqli_query($conn, $fetch_blog_posts) or die("Query Failed");
+
+                    if(mysqli_num_rows($fetch_blog_posts_query) > 0) {
+                        while($row = mysqli_fetch_assoc($fetch_blog_posts_query)) {
+                ?>
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <img class="img-fluid" src="img/blog-1.jpg" alt="">
+                    <img class="bg-light border-bottom" src="upload-images/<?php echo $row['featured_image']; ?>" width="100%" height="310">
                     <div class="bg-light p-4">
-                        <a class="d-block h5 lh-base mb-4" href="">How to cultivate organic fruits and vegetables in own firm</a>
+                        <a class="d-block h5 lh-base mb-4" href=""><?php echo $row['title']; ?></a>
                         <div class="text-muted border-top pt-4">
-                            <small class="me-3"><i class="fa fa-user text-primary me-2"></i>Admin</small>
-                            <small class="me-3"><i class="fa fa-calendar text-primary me-2"></i>01 Jan, 2045</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <img class="img-fluid" src="img/blog-2.jpg" alt="">
-                    <div class="bg-light p-4">
-                        <a class="d-block h5 lh-base mb-4" href="">How to cultivate organic fruits and vegetables in own firm</a>
-                        <div class="text-muted border-top pt-4">
-                            <small class="me-3"><i class="fa fa-user text-primary me-2"></i>Admin</small>
-                            <small class="me-3"><i class="fa fa-calendar text-primary me-2"></i>01 Jan, 2045</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <img class="img-fluid" src="img/blog-3.jpg" alt="">
-                    <div class="bg-light p-4">
-                        <a class="d-block h5 lh-base mb-4" href="">How to cultivate organic fruits and vegetables in own firm</a>
-                        <div class="text-muted border-top pt-4">
-                            <small class="me-3"><i class="fa fa-user text-primary me-2"></i>Admin</small>
-                            <small class="me-3"><i class="fa fa-calendar text-primary me-2"></i>01 Jan, 2045</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                            <small class="me-3"><i class="fa fa-user text-primary me-2"></i><?php echo $row['author']; ?></small>
+                            <small class="me-3"><i class="fa fa-calendar text-primary me-2"></i><?php echo $row['publish_date']; ?></small>
+                        </div><!--text-muted-->
+                    </div><!--bg-light-->
+                </div><!--col-lg-4-->
+                <?php 
+                        }
+                    }    
+                ?>
+            </div><!--row-->
+        </div><!--container-->
+    </div><!--container-xxl-->
     <!-- Blog End -->
 
     <?php require 'template-parts/footer.php'; ?>
