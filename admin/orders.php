cc56f3a42c63246cae6bb44c648179827a9bef82
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-18 18:52:06 +0500

    Creating new page for showing all orders...

---
+++ admin/orders.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Orders - Mehar Grocery</title>
+    <?php require_once 'header_links.php'; ?>
+</head>
+<body>
+
+    <?php require_once 'header.php'; ?>
+
+    <?php
+        require_once '../config.php';
+        if(!isset($_SESSION['fname'])) {
+            header('location: ../login.php');
+        }
+    ?>
+
+    <section class="admin_section section_content wow fadeInUP">
+        <div class="container-fluid">
+            <div class="main_content">
+                <div class="row">
+                    <div class="col-lg-2 col-md-2 col-sm-12" id="admin_menu">
+                        <?php require_once 'sidebar.php'; ?>
+                    </div><!--col-lg-2-->
+                    <div class="col-lg-10 col-md-01 col-sm-12 admin_content p-3">
+                        <h1>Orders:</h1>
+                    </div><!--col-lg-10-->
+                </div><!--row-->
+            </div><!--main_content-->
+        </div><!--container-->
+    </section>
+
+    <?php require_once 'footer.php'; ?>
+    <?php require_once 'footer_links.php'; ?>
+
+</body>
+</html>
