db51caa375830e1c02729d3c8cca60a22141cfd9
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-18 19:41:49 +0500

    Adding pagination for showing limited products...

--- admin/products.php
+++ admin/products.php
@@ -44,7 +44,29 @@
                             <tbody>
                                 <?php
                                     require_once '../config.php';
-                                    $fetch_products = "SELECT * FROM `products`";
+
+                                    //define total number of results you want per page  
+                                    $results_per_page = 10;
+                                
+                                    //find the total number of results stored in the database  
+                                    $query = "SELECT * FROM `products`";
+                                    $result = mysqli_query($conn, $query);
+                                    $number_of_result = mysqli_num_rows($result);
+                                
+                                    //determine the total number of pages available  
+                                    $number_of_page = ceil ($number_of_result / $results_per_page);  
+                                
+                                    //determine which page number visitor is currently on  
+                                    if (isset($_GET['page'])) {
+                                        $page = $_GET['page'];
+                                    } else {  
+                                        $page = 1;
+                                    }  
+                                
+                                    //determine the sql LIMIT starting number for the results on the displaying page  
+                                    $page_first_result = ($page-1) * $results_per_page;
+                                    
+                                    $fetch_products = "SELECT * FROM `products` LIMIT " . $page_first_result . ',' . $results_per_page;
                                     $fetch_products_query = mysqli_query($conn, $fetch_products) or die("Query Failed");
                                     $sr = 1;
 
@@ -102,6 +124,19 @@
                                 ?>
                             </tbody>
                         </table>
+                        <div class="pagination">
+                        <?php
+                            if($page >= 2){ ?>
+                                <a href="products.php?page=<?php echo $page-1; ?>" class="btn btn-success mx-2">Prev</a>
+                            <?php }
+                            for($page = 1; $page <= $number_of_page; $page++) { ?>
+                                <a href="products.php?page=<?php echo $page; ?>" class="btn btn-success mx-2 <?php if($_GET["page"] == $page) echo "current" ?>"><?php echo $page; ?></a>
+                            <?php }
+                            if($page < $number_of_page){ ?>
+                                <a href="products.php?page=<?php echo $page=1; ?>" class="btn btn-success mx-2">Next</a>
+                            <?php }
+                        ?>
+                        </div><!--pagination-->
                     </div><!--col-lg-10-->
                 </div><!--row-->
             </div><!--main_content-->
