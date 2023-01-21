eedb9add750869b5a5e0bf23359dffd44c0a1ed7
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-21 19:34:01 +0500

    Adding error message...

--- index.php
+++ index.php
@@ -12,7 +12,11 @@
 
 <body>
     
-    <?php require 'template-parts/header.php'; if(isset($_SESSION['success'])) echo $_SESSION['success']; ?>
+    <?php 
+        if(isset($_SESSION['success'])) echo $_SESSION['success']; 
+        if(isset($_SESSION['error'])) echo $_SESSION['error'];
+        require 'template-parts/header.php';
+    ?>
 
     <!-- Carousel Start -->
     <div class="container-fluid p-0 mb-5 wow fadeIn" data-wow-delay="0.1s">
