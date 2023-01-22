2b124df352bf8225f2095704b87e1ef3c946ff4c
AmirAliLiaqat <amirliaqat2020@gmail.com>
2023-01-22 20:42:07 +0500

    Changing site main name in all of these files...

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Register - <?php session_start(); echo $_SESSION['site_name']; ?></title>
+    <title>Register - <?php session_start(); if(isset($_SESSION['site_name'])) { echo $_SESSION['site_name']; } else {echo "Mehar Grocery";} ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
--- view_user.php
+++ view_user.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View User - <?php session_start(); echo $_SESSION['site_name']; ?></title>
+    <title>View User - <?php session_start(); if(isset($_SESSION['site_name'])) { echo $_SESSION['site_name']; } else {echo "Mehar Grocery";} ?></title>
     <?php require_once 'template-parts/header-links.php'; ?>
 </head>
 <body>
